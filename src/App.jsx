550cc1c3d0d0aabdba4116ad48b04a8e65e7029e
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-22 21:44:55 -0400

    add BrowserRouter, add a Nav and Home components, and display Nav and Home in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Home from "./components/Home";
+import Nav from "./common/Nav";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return <div>
+      <Nav />
+      <Home />
+    </div>;
 };
 
 export default App;
